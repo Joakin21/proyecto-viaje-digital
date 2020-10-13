c6666bd13071a41c94ad363150877421bbd7dca1
Carlos <karloz.97@gmail.com>
2020-10-13 14:23:24 -0300

    se mejora formularios de crud de usuarios profesionales de la salud

--- viaje-digital/src/app/crud-profesionales-salud/crud-profesionales-salud.component.ts
+++ viaje-digital/src/app/crud-profesionales-salud/crud-profesionales-salud.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../servicios/user.service';
 import { NgForm, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 declare var $: any;
 
@@ -11,20 +12,16 @@ declare var $: any;
 })
 export class CrudProfesionalesSaludComponent implements OnInit {
 
-  constructor(private userService: UserService) { }
-
-  updateUserForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    first_name: new FormControl(''),
-    last_name: new FormControl(''),
-    profesion: new FormControl(''),
-    centro_salud: new FormControl('')
-  });
+  constructor(private userService: UserService, private formBuilder: FormBuilder) { }
+  crearteUserForm: FormGroup
+  updateUserForm: FormGroup
 
   myUsers: any[] = []
-  show_error_create_user: boolean = false
+  show_error_create_user: boolean = true
+  show_error_update_user: boolean = false
   error_create_user: string = ""
+
+  index_user_to_update: number 
 
   ngOnInit(): void {
     //Obtengo todos los usuarios
@@ -37,23 +34,42 @@ export class CrudProfesionalesSaludComponent implements OnInit {
         console.log('error', error)
       }
     );
+
+    this.crearteUserForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+      repeat_password: ['', Validators.required],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      profesion: ['', Validators.required],
+      centro_salud: ['', Validators.required]
+    })
+
+    this.updateUserForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: [''],
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      profesion: ['', Validators.required],
+      centro_salud: ['', Validators.required]
+    })
+
   }
 
-  createUser(new_user: NgForm) {
-    var valid_data = new_user.valid
-    var form_user = new_user.value
 
-    if (!valid_data) {
-      this.error_create_user = "Error, you must fill all the fields with the required information"
-      this.show_error_create_user = true
-    }
-    else if (form_user["password"] != form_user["password_rep"]) {
+  createUser() {
+    console.log(this.crearteUserForm.value)
+    let form_user = this.crearteUserForm.value
+    
+    //this.passwords_match = new_user['password'] == new_user['repeat_password'] 
+    if (form_user["password"] != form_user["repeat_password"]) {
       this.error_create_user = "Error, passwords do not match"
       this.show_error_create_user = true
     }
+  
     else {
       //preparamos los datos para crear nuevo usuario
-      var my_new_user =
+      let my_new_user =
       {
         "user":
         {
@@ -110,11 +126,13 @@ export class CrudProfesionalesSaludComponent implements OnInit {
 
   }
 
-  index_user_to_update: number
   prepareModalUpdateUser(user_index) {
+
     this.index_user_to_update = user_index
     //seteamos el form del modal update con los calores del usuario seleccionado
     this.updateUserForm.controls["username"].setValue(this.myUsers[user_index]["user"]["username"])
+    this.crearteUserForm.controls["password"].setValue('')
+
     this.updateUserForm.controls["first_name"].setValue(this.myUsers[user_index]["user"]["first_name"])
     this.updateUserForm.controls["last_name"].setValue(this.myUsers[user_index]["user"]["last_name"])
     this.updateUserForm.controls["profesion"].setValue(this.myUsers[user_index]["profesion"])
@@ -124,39 +142,38 @@ export class CrudProfesionalesSaludComponent implements OnInit {
   }
 
   updateUser() {
-    $('#modalUpdateUser').modal('toggle');
+    //alert("AAAAAAAAAAAAAAAAAA!!!")
+    console.log(this.updateUserForm.value)
+    
+    let updateUserForm = this.updateUserForm.value
 
-    var user_id = this.myUsers[this.index_user_to_update]["user"]["id"]
-    var user_updated = {
+    let user_updated = {
       "user": {
-      }
+        "first_name":updateUserForm["first_name"],
+        "last_name":updateUserForm["last_name"],        
+      },
+      "profesion":updateUserForm["profesion"],
+      "centro_salud":updateUserForm["centro_salud"],
     }
-    var updateUserForm = this.updateUserForm.value
-
-    if (updateUserForm["username"]) {
-      user_updated["user"]["username"] = updateUserForm["username"]
-    }
-    if (updateUserForm["password"]) {
+    if(updateUserForm["password"] != ""){
       user_updated["user"]["password"] = updateUserForm["password"]
     }
-    if (updateUserForm["first_name"]) {
-      user_updated["user"]["first_name"] = updateUserForm["first_name"]
+    if(updateUserForm["username"] != this.myUsers[this.index_user_to_update]["user"]["username"]){
+      user_updated["user"]["username"] = updateUserForm["username"]
     }
-    if (updateUserForm["last_name"]) {
-      user_updated["user"]["last_name"] = updateUserForm["last_name"]
-    }
-    if (updateUserForm["profesion"]) {
-      user_updated["profesion"] = updateUserForm["profesion"]
-    }
-    if (updateUserForm["centro_salud"]) {
-      user_updated["centro_salud"] = updateUserForm["centro_salud"]
-    }
-
+  
+    let user_id = this.myUsers[this.index_user_to_update]["user"]["id"]
+  
     this.userService.updateUser(user_updated, user_id).subscribe(
       data => {
         //console.log(data)
         if (!data["detail"]) {//sin no hay un error inesperado
+          this.show_error_update_user = false
           this.myUsers[this.index_user_to_update] = data
+          $('#modalUpdateUser').modal('toggle');
+        }else{
+          this.show_error_update_user = true
+          //alert(data["detail"])
         }
       },
       error => {
